8490be47810b8f728e98b4ce2abc172a978ed27c
electricSoda <justin.hua.ge@gmail.com>
2023-08-19 17:14:12 -0600

    Update template

--- TEMPLATE.cpp
+++ TEMPLATE.cpp
@@ -21,8 +21,8 @@ using vi = vector<int>;
 // Debug
 template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
 template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
-void dbg_out() { cout << nl; }
-template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cout << H << ' '; dbg_out(T...); }
+void dbg() { cout << nl; }
+template<typename Head, typename... Tail> void dbg(Head H, Tail... T) { cout << H << ' '; dbg(T...); }
 #define de(...) cerr << "(" << #__VA_ARGS__ << "): ", dbg_out(__VA_ARGS__)
 
 /*----------------------------------------------------------------*/

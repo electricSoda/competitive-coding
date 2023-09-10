620dcd10cbc1525bd26ce0bd42accad6b282a834
electricSoda <justin.hua.ge@gmail.com>
2023-09-10 14:26:21 -0600

    Removed redundant pragmas

--- TEMPLATE.cpp
+++ TEMPLATE.cpp
@@ -1,7 +1,3 @@
-#pragma GCC optimize("Ofast,no-stack-protector,unroll-loops,fast-math,O3")
-#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx")
-#pragma GCC optimize("Ofast")
-#pragma GCC target("avx,avx2,fma")
 #include <bits/stdc++.h>
 
 using namespace std;
